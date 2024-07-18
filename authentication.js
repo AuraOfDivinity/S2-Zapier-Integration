ee6d426c4925e177c57197ca25b19e9ecd415d5d
auraofdivinity <auraofdivinity@gmail.com>
2024-07-18 23:52:04 +0530

    Removing redirect uri

--- authentication.js
+++ authentication.js
@@ -55,7 +55,6 @@ const refreshAccessToken = async (z, bundle) => {
       client_secret: process.env.CLIENT_SECRET,
       grant_type: "refresh_token",
       refresh_token: bundle.authData.refresh_token,
-      redirect_uri: bundle.inputData.redirect_uri,
     },
     headers: { "content-type": "application/x-www-form-urlencoded" },
   });
